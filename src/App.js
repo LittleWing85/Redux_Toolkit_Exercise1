8fc8d0423d841fd7e5ac766aca965f72643ddccd
littlewing <mail@alexandra-jaeger.net>
2022-12-09 22:52:45 +0100

    practising redux

--- src/App.js
+++ src/App.js
@@ -1,27 +1,36 @@
-import logo from "./logo.svg";
-import "./App.css";
-import { Counter } from "./features/counter/Counter";
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom'
+
+import { Navbar } from './app/Navbar'
+import { PostsList } from './features/posts/PostsList'
+import { AddPostForm } from './features/posts/AddPostForm'
 
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <Counter />
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
+  return (
+    <Router>
+      <Navbar />
+      <div className="App">
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <React.Fragment>
+                <AddPostForm />
+                <PostsList />
+              </React.Fragment>
+            )}
+          />
+          <Redirect to="/" />
+        </Switch>
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
---
+++ src/features/posts/postsSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = [
+  { id: '1', title: 'First post', content: 'Hello!' },
+  {
+    id: '2',
+    title: 'Second post',
+    content: 'I would like to see some cute animal pictures!',
+  },
+]
+const postsSlice = createSlice({
+  name: 'posts',
+  initialState,
+  reducers: {
+    postAdded(state, action) {
+      state.push(action.payload)
+    },
+  },
+})
+
+export const { postAdded } = postsSlice.actions
+
+export default postsSlice.reducer
